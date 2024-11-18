bb4e664bd56d14cb79a21ffdc33379654442c069
TheLionCoder <eorlando.reyes@icloud.com>
2024-11-18 16:05:30 -0500

    add record struct file

---
+++ src/record_context.rs
@@ -0,0 +1,32 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
+use csv::StringRecord;
+use crate::delimiter::Delimiter;
+
+#[derive(Clone)]
+pub(crate) struct RecordProcessingContext {
+    pub(crate) headers: StringRecord,
+    pub(crate) output_dir: PathBuf,
+    pub(crate) create_directory: bool,
+    pub(crate) file_name: String,
+    pub(crate) delimiter: u8,
+    pub(crate) split_column_idx: usize,
+    pub(crate) writers: Arc<Mutex<HashMap<String, csv::Writer<BufWriter<File>>>>>
+}
+
+impl Default for RecordProcessingContext{
+    fn default() -> Self {
+        RecordProcessingContext {
+            headers: StringRecord::new(),
+            output_dir: PathBuf::new(),
+            create_directory: false,
+            file_name: String::new(),
+            delimiter: Delimiter::PIPE,
+            split_column_idx: 0,
+            writers: Arc::new(Mutex::new(HashMap::new()))
+        }
+    }
+}
